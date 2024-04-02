0a22021c0c78ab7b0b51980b8367e171923bc36d
Maxim Kupfer <mbkupfer@gmail.com>
2024-04-02 09:54:37 -0700

    Rough concept
    Created a rough concept, but still need to workshop how to approach better flexibility in google querying options

--- search.py
+++ search.py
@@ -21,11 +21,11 @@ SITES = [
 ]
 
 
-def create_url(site: str, job_title: str, query_date_range: str) -> str:
+def create_url(site: str, job_title: str, query_date_range: str, custom_query: str) -> str:
     base_url = "https://www.google.com/search?"
     query_string = urllib.parse.urlencode(
         {
-            "q": f'"{job_title}"',
+            "q": f'"{job_title}" {custom_query}',
             "as_sitesearch": site,
             "tbs": f"qdr:{query_date_range}",
         },
@@ -34,13 +34,15 @@ def create_url(site: str, job_title: str, query_date_range: str) -> str:
     return base_url + query_string
 
 
-def main(job_title: str, query_date_range: str, sites_fp=None) -> None:
+def main(job_title: str, query_date_range: str, sites_fp=None, custom_query: str = None) -> None:
+    if custom_query is None:
+        custom_query = ''
     if not sites_fp:
         sites = SITES
     else:
         sites = [ln.strip() for ln in sites_fp.readlines()]
     for site in sites:
-        webbrowser.open_new_tab(create_url(site, job_title, query_date_range))
+        webbrowser.open_new_tab(create_url(site, job_title, query_date_range, custom_query))
 
 
 if __name__ == "__main__":
@@ -62,9 +64,15 @@ if __name__ == "__main__":
         type=argparse.FileType("r"),
         help="Text file for customizing the sites that are used.",
     )
+    parser.add_argument(
+        "--custom-query",
+        help="",
+    )
     args = parser.parse_args()
+    
     main(
         job_title=args.job_title,
         query_date_range=args.query_date_range,
         sites_fp=args.sites,
+        custom_query=args.custom_query,
     )
